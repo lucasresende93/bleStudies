7334e4d507dfae2d30f71ae793b7ba6b3b1062f6
Lucas <lucas.resende.eng@gmail.com>
2024-05-23 10:20:35 -0300

    edit: changed Init screen

--- src/navigators/mainStack.tsx
+++ src/navigators/mainStack.tsx
@@ -19,7 +19,7 @@ const StackComponent = () => {
   return (
     <NavigationContainer>
       <BluetoothProvider>
-        <Stack.Navigator initialRouteName="ble">
+        <Stack.Navigator initialRouteName="Login">
           <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
           <Stack.Screen name="ble" component={BleWindow2} options={{ headerTitle: LogoTitle, headerBackVisible: false }} />
         </Stack.Navigator>
